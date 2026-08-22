b5aeff88367deea513ef09505be1f57e857ca1f1
Emma Prince <dopetone701@gmail.com>
2026-08-22 23:35:35 +0400

    real sw + ios meta

--- sw.js
+++ sw.js
@@ -1,2 +1,9 @@
-self.addEventListener('install', e => self.skipWaiting());
-self.addEventListener('activate', e => self.clients.claim());
+const CACHE = "dope-vault-v2";
+self.addEventListener("install", e => {
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(["./", "./index.html", "./manifest-data-uri.webmanifest"])));
+  self.skipWaiting();
+});
+self.addEventListener("activate", e => self.clients.claim());
+self.addEventListener("fetch", e => {
+  e.respondWith(caches.match(e.request).then(r => r || fetch(e.request)));
+});
